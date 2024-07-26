9cd4ddc348eceb449646654a94206ae13ea08a8e
KiSeungMin <ksm011212@gmail.com>
2024-07-26 16:07:56 +0900

    add column name in userId

--- src/main/java/com/aisip/OnO/backend/entity/User.java
+++ src/main/java/com/aisip/OnO/backend/entity/User.java
@@ -1,9 +1,6 @@
 package com.aisip.OnO.backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +10,7 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
     private String email;
