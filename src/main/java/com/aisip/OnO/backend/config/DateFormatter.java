b5ae327b90be77860d27f7ce13828482d448150f
KiSeungMin <ksm011212@gmail.com>
2024-09-02 16:30:56 +0900

    ui enhancement : admin page date format change

---
+++ src/main/java/com/aisip/OnO/backend/config/DateFormatter.java
@@ -0,0 +1,15 @@
+package com.aisip.OnO.backend.config;
+
+import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+@Component
+public class DateFormatter {
+
+    public String format(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초", Locale.KOREAN);
+        return dateTime.format(formatter);
+    }
+}
