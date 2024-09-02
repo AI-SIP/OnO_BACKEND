16a08e9f93549a56f077536fe66d85ffdd5b5871
KiSeungMin <ksm011212@gmail.com>
2024-09-02 16:51:13 +0900

    hot fix : null check on dateformat

--- src/main/java/com/aisip/OnO/backend/config/DateFormatter.java
+++ src/main/java/com/aisip/OnO/backend/config/DateFormatter.java
@@ -9,6 +9,10 @@ import java.util.Locale;
 public class DateFormatter {
 
     public String format(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            return "날짜 정보 없음";  // null일 때 반환할 기본 메시지 또는 다른 값을 설정할 수 있습니다.
+        }
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초", Locale.KOREAN);
         return dateTime.format(formatter);
     }
