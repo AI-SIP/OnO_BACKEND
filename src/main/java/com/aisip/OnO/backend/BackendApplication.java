aacec1a2d5f990976d5c592d8fe1c1ed9969d2ec
KiSeungMin <ksm011212@gmail.com>
2024-09-09 18:24:22 +0900

    add entity scan annotation

--- src/main/java/com/aisip/OnO/backend/BackendApplication.java
+++ src/main/java/com/aisip/OnO/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package com.aisip.OnO.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EntityScan(basePackages = "com.aisip.OnO.backend.entity")
 public class BackendApplication {
 
 	public static void main(String[] args) {
