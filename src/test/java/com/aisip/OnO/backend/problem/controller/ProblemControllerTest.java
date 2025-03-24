159b207ddf740ed8c991ccff7f7d9e31e2f54b64
KiSeungMin <ksm011212@gmail.com>
2025-03-24 20:06:34 +0900

    test: problem controller test 테스트 데이터 생성 및 특정 문제 조회 기능 구현 완료

---
+++ src/test/java/com/aisip/OnO/backend/problem/controller/ProblemControllerTest.java
@@ -0,0 +1,131 @@
+package com.aisip.OnO.backend.problem.controller;
+
+import com.aisip.OnO.backend.auth.WithMockCustomUser;
+import com.aisip.OnO.backend.problem.dto.ProblemImageDataResponseDto;
+import com.aisip.OnO.backend.problem.dto.ProblemResponseDto;
+import com.aisip.OnO.backend.problem.entity.ProblemImageType;
+import com.aisip.OnO.backend.problem.service.ProblemService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProblemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProblemService problemService;
+
+    private List<ProblemResponseDto> problemResponseDtoList;
+
+    @BeforeEach
+    void setUp() {
+        problemResponseDtoList = new ArrayList<>();
+
+        for(int i = 1; i <= 5; i++){
+            List<ProblemImageDataResponseDto> imageUrlList = new ArrayList<>();
+            for(int j = 1; j <= 3; j++){
+                ProblemImageDataResponseDto problemImageDataResponseDto = new ProblemImageDataResponseDto(
+                        "imageUrl_" + i + "_" + j,
+                        ProblemImageType.valueOf(j),
+                        LocalDateTime.now()
+                );
+
+                imageUrlList.add(problemImageDataResponseDto);
+            }
+
+            ProblemResponseDto problemResponseDto = new ProblemResponseDto(
+                    (long)i,
+                    "memo" + i,
+                    "reference" + i,
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    imageUrlList
+            );
+
+            problemResponseDtoList.add(problemResponseDto);
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @DisplayName("특정 문제 조회")
+    @WithMockCustomUser(userId = 1L, role = "ROLE_MEMBER")
+    void getProblem() throws Exception{
+        //given
+        Long problemId = 1L;
+        given(problemService.findProblem(problemId, 1L)).willReturn(problemResponseDtoList.get(0));
+
+        // When & Then
+        mockMvc.perform(get("/api/problem/"+problemId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.problemId").value(1L))
+                .andExpect(jsonPath("$.data.memo").value("memo1"))
+                .andExpect(jsonPath("$.data.reference").value("reference1"))
+                .andExpect(jsonPath("$.data.imageUrlList[0].imageUrl").value("imageUrl_1_1"))
+                .andExpect(jsonPath("$.data.imageUrlList[0].problemImageType").value("PROBLEM_IMAGE"));
+    }
+
+    @Test
+    void getProblemsByUserId() {
+    }
+
+    @Test
+    void getUserProblemCount() {
+    }
+
+    @Test
+    void registerProblem() {
+    }
+
+    @Test
+    void registerProblemImageData() {
+    }
+
+    @Test
+    void updateProblemInfo() {
+    }
+
+    @Test
+    void updateProblemPath() {
+    }
+
+    @Test
+    void updateProblemImageData() {
+    }
+
+    @Test
+    void deleteProblems() {
+    }
+
+    @Test
+    void deleteProblemImageData() {
+    }
+}
