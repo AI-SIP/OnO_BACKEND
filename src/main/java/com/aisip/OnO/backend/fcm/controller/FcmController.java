52abb7c46826d38f3cbba621873624ee770b7d9f
KiSeungMin <ksm011212@gmail.com>
2025-05-22 16:36:48 +0900

    feat: 유저가 보낸 FCM 토큰 수신 구현 완료

---
+++ src/main/java/com/aisip/OnO/backend/fcm/controller/FcmController.java
@@ -0,0 +1,26 @@
+package com.aisip.OnO.backend.fcm.controller;
+
+import com.aisip.OnO.backend.common.response.CommonResponse;
+import com.aisip.OnO.backend.fcm.dto.FcmTokenRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/fcm")
+public class FcmController {
+
+    @PostMapping("/token")
+    public CommonResponse<String> registerFcmToken(@RequestBody FcmTokenRequestDto fcmTokenRequestDto) {
+        Long userId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        log.info("fcm token: {}", fcmTokenRequestDto.token());
+
+        return CommonResponse.success("문제가 등록되었습니다.");
+    }
+}
---
+++ src/main/java/com/aisip/OnO/backend/fcm/dto/FcmTokenRequestDto.java
@@ -0,0 +1,6 @@
+package com.aisip.OnO.backend.fcm.dto;
+
+public record FcmTokenRequestDto (
+        String token
+){
+}
