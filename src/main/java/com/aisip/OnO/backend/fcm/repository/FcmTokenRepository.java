e9d1f02c74fdf414af2991f7d54b6bc240843389
KiSeungMin <ksm011212@gmail.com>
2025-05-22 16:51:08 +0900

    feat: FcmTokenRepository 생성 완료

---
+++ src/main/java/com/aisip/OnO/backend/fcm/repository/FcmTokenRepository.java
@@ -0,0 +1,13 @@
+package com.aisip.OnO.backend.fcm.repository;
+
+import com.aisip.OnO.backend.fcm.entity.FcmToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
+
+    boolean existsByToken(String token);
+
+    Optional<FcmToken> findByToken(String token);
+}
