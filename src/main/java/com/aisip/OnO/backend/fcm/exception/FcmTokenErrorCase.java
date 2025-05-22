f5aeed40bcbd5e63d2e5d536c1be52408e571055
KiSeungMin <ksm011212@gmail.com>
2025-05-22 16:55:19 +0900

    feat: FcmTokenService 생성 완료

---
+++ src/main/java/com/aisip/OnO/backend/fcm/exception/FcmTokenErrorCase.java
@@ -0,0 +1,16 @@
+package com.aisip.OnO.backend.fcm.exception;
+
+import com.aisip.OnO.backend.common.exception.ErrorCase;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum FcmTokenErrorCase implements ErrorCase {
+
+    FCM_TOKEN_NOT_FOUND(400, 7001, "Fcm Token을 찾을 수 없습니다.");
+
+    private final Integer httpStatusCode;
+    private final Integer errorCode;
+    private final String message;
+}
