d60b1695b25852c5ff8b356739eb3f980d45a913
KiSeungMin <ksm011212@gmail.com>
2025-06-30 23:43:56 +0900

    feat: MissionType Enum에 point 필드 추가

--- src/main/java/com/aisip/OnO/backend/mission/entity/MissionType.java
+++ src/main/java/com/aisip/OnO/backend/mission/entity/MissionType.java
@@ -1,8 +1,17 @@
 package com.aisip.OnO.backend.mission.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum MissionType {
-    USER_LOGIN,
-    PROBLEM_WRITE,
-    PROBLEM_PRACTICE,
-    NOTE_PRACTICE,
+    USER_LOGIN(5L),
+    PROBLEM_WRITE(5L),
+    PROBLEM_PRACTICE(10L),
+    NOTE_PRACTICE(15L);
+
+    private final Long point;
+
+    MissionType(Long point) {
+        this.point = point;
+    }
 }
