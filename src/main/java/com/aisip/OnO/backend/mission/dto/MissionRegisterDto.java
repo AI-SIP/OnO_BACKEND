62602eb296a428ae0cc8294c65d01195c6e38d27
KiSeungMin <ksm011212@gmail.com>
2025-07-01 01:19:38 +0900

    feat: MissionRegisterDto에 Builder 적용

--- src/main/java/com/aisip/OnO/backend/mission/dto/MissionRegisterDto.java
+++ src/main/java/com/aisip/OnO/backend/mission/dto/MissionRegisterDto.java
@@ -1,7 +1,9 @@
 package com.aisip.OnO.backend.mission.dto;
 
 import com.aisip.OnO.backend.mission.entity.MissionType;
+import lombok.Builder;
 
+@Builder
 public record MissionRegisterDto (
     Long userId,
     MissionType missionType,
