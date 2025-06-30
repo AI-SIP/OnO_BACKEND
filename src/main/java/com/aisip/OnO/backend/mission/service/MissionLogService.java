bfea90122d433e63dc20a95669f29f54cf4164ba
KiSeungMin <ksm011212@gmail.com>
2025-07-01 00:49:34 +0900

    feat: MissionLogService 생성 로직 응답 형식 변경

--- src/main/java/com/aisip/OnO/backend/mission/service/MissionLogService.java
+++ src/main/java/com/aisip/OnO/backend/mission/service/MissionLogService.java
@@ -22,7 +22,7 @@ public class MissionLogService {
 
     private static final Long DAILY_MISSION_POINT_LIMIT = 100L;
 
-    public void registerMissionLog(MissionRegisterDto missionRegisterDto) {
+    public String registerMissionLog(MissionRegisterDto missionRegisterDto) {
 
         boolean canRegister = true;
         canRegister = switch (missionRegisterDto.missionType()) {
@@ -44,11 +44,21 @@ public class MissionLogService {
             if(pointToday <= DAILY_MISSION_POINT_LIMIT) {
                 Long point = getMin(missionLog.getPoint(), DAILY_MISSION_POINT_LIMIT - pointToday);
                 user.getUserMissionStatus().gainPoint(point);
+
+                return point + "점을 획득했습니다!";
+            } else {
+                return "일일 포인트 최대치를 획득했습니다.";
             }
         }
+
+        return "미션 등록에 실패했습니다";
+    }
+
+    public void getUserMissionLog(Long userId) {
+
     }
 
     private Long getMin(Long p1, Long p2) {
-        return p1 > p2 ? p1 : p2;
+        return p1 > p2 ? p2 : p1;
     }
 }
