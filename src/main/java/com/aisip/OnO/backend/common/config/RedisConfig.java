f196579fb3065718ca2756bcd23306141e6a3f9c
KiSeungMin <ksm011212@gmail.com>
2025-10-13 18:38:31 +0900

    [chore] Redis 설치 및 설정 완료

---
+++ src/main/java/com/aisip/OnO/backend/common/config/RedisConfig.java
@@ -0,0 +1,93 @@
+package com.aisip.OnO.backend.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    /**
+     * Redis 연결을 위한 'Connection' 생성합니다.
+     *
+     * @return RedisConnectionFactory
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    /**
+     * Redis 데이터 처리를 위한 템플릿을 구성합니다.
+     * 해당 구성된 RedisTemplate을 통해서 데이터 통신으로 처리되는 대한 직렬화를 수행합니다.
+     *
+     * @return RedisTemplate<String, Object>
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        // Redis를 연결합니다.
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        // Key-Value 형태로 직렬화를 수행합니다.
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        // Hash Key-Value 형태로 직렬화를 수행합니다.
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+        // 기본적으로 직렬화를 수행합니다.
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    /**
+     * 리스트에 접근하여 다양한 연산을 수행합니다.
+     *
+     * @return ListOperations<String, Object>
+     */
+    public ListOperations<String, Object> getListOperations() {
+        return this.redisTemplate().opsForList();
+    }
+
+    /**
+     * 단일 데이터에 접근하여 다양한 연산을 수행합니다.
+     *
+     * @return ValueOperations<String, Object>
+     */
+    public ValueOperations<String, Object> getValueOperations() {
+        return this.redisTemplate().opsForValue();
+    }
+
+
+    /**
+     * Redis 작업중 등록, 수정, 삭제에 대해서 처리 및 예외처리를 수행합니다.
+     *
+     * @param operation
+     * @return
+     */
+    public int executeOperation(Runnable operation) {
+        try {
+            operation.run();
+            return 1;
+        } catch (Exception e) {
+            System.out.println("Redis 작업 오류 발생 :: " + e.getMessage());
+            return 0;
+        }
+    }
+}
