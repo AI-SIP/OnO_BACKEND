f6b42f06c3105e835d169584ecb6a04f7b0d8fa0
KiSeungMin <ksm011212@gmail.com>
2025-10-15 18:37:05 +0900

    [feat] 비동기 처리가 가능하도록 Config 구현

---
+++ src/main/java/com/aisip/OnO/backend/common/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package com.aisip.OnO.backend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    // Spring의 @Async 기능을 활성화
+}
