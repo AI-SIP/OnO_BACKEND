2ee5a4283fc06d331f7137e8555a21ac392d53a3
KiSeungMin <ksm011212@gmail.com>
2025-12-26 23:50:31 +0900

    [fix] Health Check api 요청 권한이 막혀있는 문제 해결

--- src/main/java/com/aisip/OnO/backend/common/auth/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/aisip/OnO/backend/common/auth/CustomAuthenticationEntryPoint.java
@@ -13,6 +13,19 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+
+        String requestURI = request.getRequestURI();
+        if(requestURI.startsWith("/actuator/health") ||
+            requestURI.startsWith("/api/auth") ||
+                requestURI.equals("/") ||
+                requestURI.equals("/robots.txt") ||
+                requestURI.equals("/home") ||
+                requestURI.startsWith("/login") ||
+                requestURI.startsWith("/swagger-ui") ||
+                requestURI.startsWith("/v3/api-docs")
+        ) {
+            return;
+        }
         String errorMessage = (String) request.getAttribute("errorMessage");
 
         if(errorMessage == null)
