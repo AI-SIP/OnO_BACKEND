153514057847fd0af3d94ac7625a3031f3c5270b
KiSeungMin <ksm011212@gmail.com>
2025-12-27 00:09:33 +0900

    [fix] Health Check api 요청 권한이 막혀있는 문제 해결

--- src/main/java/com/aisip/OnO/backend/auth/config/SecurityConfig.java
+++ src/main/java/com/aisip/OnO/backend/auth/config/SecurityConfig.java
@@ -61,9 +61,8 @@ public class SecurityConfig {
 
         http.authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/", "/robots.txt", "/home","/images/**", "/login", "/css/**", "/js/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .requestMatchers("/", "/robots.txt", "/home","/images/**", "/login", "/css/**", "/js/**", "/swagger-ui/**", "/v3/api-docs/**", "/actuator/**").permitAll()
                                 .requestMatchers("/api/auth/**").permitAll()
-                                .requestMatchers("/actuator/health").permitAll()
                                 .requestMatchers("/admin/**").hasRole("ADMIN")
                                 .requestMatchers("/api/fcm/**").hasAnyRole("GUEST", "MEMBER", "ADMIN")
                                 .requestMatchers("/api/users/**").hasAnyRole("GUEST", "MEMBER", "ADMIN")
