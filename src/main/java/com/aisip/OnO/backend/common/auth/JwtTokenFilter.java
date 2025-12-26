1199b7fec43226b23b5f6ffce421f1d5738dd209
KiSeungMin <ksm011212@gmail.com>
2025-12-27 00:21:37 +0900

    [fix] Health Check api 요청 권한이 막혀있는 문제 해결

--- src/main/java/com/aisip/OnO/backend/common/auth/JwtTokenFilter.java
+++ src/main/java/com/aisip/OnO/backend/common/auth/JwtTokenFilter.java
@@ -36,7 +36,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         String path = request.getRequestURI();
 
         // JWT 필터를 건너뛸 경로들
-        return path.startsWith("/actuator/health") ||
+        return path.startsWith("/actuator/") ||
                  path.startsWith("/api/auth/") ||
                  path.equals("/") ||
                  path.equals("/robots.txt") ||
