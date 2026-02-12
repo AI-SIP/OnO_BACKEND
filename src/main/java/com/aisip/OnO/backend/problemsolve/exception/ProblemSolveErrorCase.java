0d0ad43f3db84bac026ac506b90b50dd3f854c29
KiSeungMin <ksm011212@gmail.com>
2026-02-12 22:56:52 +0900

    [chore] 복습 기록 예외 메시지 수정

--- src/main/java/com/aisip/OnO/backend/problemsolve/exception/ProblemSolveErrorCase.java
+++ src/main/java/com/aisip/OnO/backend/problemsolve/exception/ProblemSolveErrorCase.java
@@ -3,16 +3,15 @@ package com.aisip.OnO.backend.problemsolve.exception;
 import com.aisip.OnO.backend.common.exception.ErrorCase;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum ProblemSolveErrorCase implements ErrorCase {
 
-    PRACTICE_RECORD_NOT_FOUND(HttpStatus.NOT_FOUND, 5001, "연습 기록을 찾을 수 없습니다."),
-    PRACTICE_RECORD_USER_UNMATCHED(HttpStatus.FORBIDDEN, 5002, "해당 연습 기록에 대한 권한이 없습니다.");
+    PROBLEM_SOLVE_NOT_FOUND(400, 4021, "복습 기록을 찾을 수 없습니다."),
+    PROBLEM_SOLVE_USER_UNMATCHED(400, 4022, "해당 복습 기록에 대한 권한이 없습니다.");
 
-    private final HttpStatus httpStatus;
-    private final Integer code;
+    private final Integer httpStatusCode;
+    private final Integer errorCode;
     private final String message;
 }
